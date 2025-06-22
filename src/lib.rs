2be5341ba2bde41c90703376dcf079afa2b7f549
BiswajitThakur <biswajit733127@gmail.com>
2025-06-22 08:24:14 +0530

    support-tilde-home-dir-for-windows

--- src/lib.rs
+++ src/lib.rs
@@ -606,11 +606,23 @@ impl Pattern {
         let mut is_recursive = false;
         let mut i = 0;
 
-        #[cfg(not(windows))]
         if let (Some(first_char), second_char) = (chars.first(), chars.get(1)) {
+            #[cfg(not(windows))]
             match (*first_char, second_char) {
                 ('~', None) | ('~', Some('/')) => {
                     if let Ok(home_dir) = std::env::var("HOME") {
+                        for ch in home_dir.chars() {
+                            tokens.push(PatternToken::Char(ch));
+                        }
+                        i += 1;
+                    }
+                }
+                _ => {}
+            }
+            #[cfg(windows)]
+            match (*first_char, second_char) {
+                ('~', None) | ('~', Some('/')) | ('~', Some('\\')) => {
+                    if let Ok(home_dir) = std::env::var("USERPROFILE") {
                         for ch in home_dir.chars() {
                             tokens.push(PatternToken::Char(ch));
                         }
