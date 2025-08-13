86419239fb661e3138573cc6ec81541a3a6dbe39
Biswajit Thakur <63996316+BiswajitThakur@users.noreply.github.com>
2025-08-13 21:29:02 +0530

    Update src/utils.rs
    Co-authored-by: Rene Leonhardt <65483435+reneleonhardt@users.noreply.github.com>

--- src/utils.rs
+++ src/utils.rs
@@ -6,9 +6,8 @@ pub(crate) fn get_home_dir() -> Option<String> {
 // This function is required when `glob_tilde_expansion` field of `glob::MatchOptions` is
 // set `true` and pattern starts with `~` followed by any char expect `/`
 pub(crate) fn get_user_name() -> Option<String> {
-    if cfg!(target_os = "linux") {
-        std::env::var("USER").ok()
-    } else {
-        None
-    }
+    #[cfg(not(target_os = "windows"))]
+    return std::env::var("USER").ok();
+    #[cfg(target_os = "windows")]
+    std::env::var("USERNAME").ok()
 }
