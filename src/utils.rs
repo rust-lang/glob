ddeeec64af060ded3b3658c2ee7cb058f19f679f
Biswajit Thakur <63996316+BiswajitThakur@users.noreply.github.com>
2025-08-13 21:31:13 +0530

    Update src/utils.rs
    Co-authored-by: Rene Leonhardt <65483435+reneleonhardt@users.noreply.github.com>

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 #[inline(always)]
 pub(crate) fn get_home_dir() -> Option<String> {
-    std::env::home_dir().map(|v| v.to_string_lossy().to_string())
+    std::env::home_dir().and_then(|v| v.to_str().map(String::from))
 }
 
 // This function is required when `glob_tilde_expansion` field of `glob::MatchOptions` is
