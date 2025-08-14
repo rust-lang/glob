498392c4263ebbda456db1b8ddb3bf38c91365c6
BiswajitThakur <biswajit733127@gmail.com>
2025-08-14 09:20:30 +0530

    .

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-extern crate glob;
-use glob::{glob_with, MatchOptions};
-
-fn main() {
-    let options = MatchOptions::new().glob_tilde_expansion(true);
-    for v in glob_with("~eagle/*", options).unwrap() {
-        println!("> {:?}", v.unwrap());
-    }
-}
