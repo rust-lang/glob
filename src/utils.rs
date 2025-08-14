e535db6b0e4a23965c0fa541b7b5493f6195f2ec
Biswajit Thakur <63996316+BiswajitThakur@users.noreply.github.com>
2025-08-14 08:02:29 +0530

    Update src/utils.rs
    Co-authored-by: Rene Leonhardt <65483435+reneleonhardt@users.noreply.github.com>

--- src/utils.rs
+++ src/utils.rs
@@ -7,7 +7,7 @@ pub(crate) fn get_home_dir() -> Option<String> {
 // set `true` and pattern starts with `~` followed by any char expect `/`
 pub(crate) fn get_user_name() -> Option<String> {
     #[cfg(not(target_os = "windows"))]
-    { std::env::var("USER").ok() }
+    return std::env::var("USER").ok();
     #[cfg(target_os = "windows")]
     std::env::var("USERNAME").ok()
 }
